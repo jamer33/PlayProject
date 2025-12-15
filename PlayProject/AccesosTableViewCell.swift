9609c60c5e86d8a10bd5fce60f386fef33a8e30b
Javier Merino <javier.merino@fluentia.com>
2025-12-15 11:05:19 +0100

    Versión final de aplicación iOS con gestión de usuarios que registra las fechas de acceso al sistema

---
+++ PlayProject/AccesosTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AccesosTableViewCell.swift
+//  PlayProject
+//
+//  Created by Mananas on 5/12/25.
+//
+
+import UIKit
+
+class AccesosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
